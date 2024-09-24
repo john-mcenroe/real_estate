3a59b996c918aaa76609a17a1f50f5575b1803f1
john-mcenroe <jmcenroe1988@gmail.com>
2024-09-24 23:07:52 +0100

    getting data in and updates to the notebooks

--- src/scrapers/my_home_metadata_enrichment.py
+++ src/scrapers/my_home_metadata_enrichment.py
@@ -230,12 +230,12 @@ def process_csv(input_file, output_file, api_key, limit=500):
             create_snapshot(output_file, ((i + 1) // 10) + 1)
 
 if __name__ == "__main__":
-    input_file = "data/processed/scraped_dublin/scraped_property_results_Dublin_page_1.csv"
+    input_file = "data/processed/scraped_dublin/scraped_property_results_Dublin_page_24.csv"
     output_file = "data/processed/scraped_dublin_metadata/scraped_property_results_metadata_Dublin_page_1.csv"
     api_key = os.getenv('GOOGLE_MAPS_API_KEY')
 
     if not api_key:
         logging.error("Google Maps API key not found. Please set the GOOGLE_MAPS_API_KEY environment variable.")
     else:
-        process_csv(input_file, output_file, api_key, limit=2000)
+        process_csv(input_file, output_file, api_key, limit=2500)
         logging.info(f"Processing complete. Results saved to {output_file}")
